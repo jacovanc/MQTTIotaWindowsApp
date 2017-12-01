bd7399ee87152d82a6bd36bd29f9dc6554f1ab38
jacovanc <jacovancr@gmail.com>
2017-12-01 17:25:29 +0000

    MQTT Receive

---
+++ MQTTIota/MQTT.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Diagnostics;
+
+namespace MQTTIota
+{
+    public class MQTT
+    {
+        public MqttClient client;
+        string messages;
+
+        public MQTT(string host, string topic)
+        {
+            client = Initialise(host, topic);
+        }
+
+        private MqttClient Initialise(string host, string topic)
+        {
+            client = new MqttClient(host);
+            client.MqttMsgPublishReceived += client_recievedMessage;
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            client.Subscribe(new String[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            return client;
+        }
+
+        static void client_recievedMessage(object sender, MqttMsgPublishEventArgs e)
+        {
+            // Handle message received
+            var message = Encoding.Default.GetString(e.Message);
+            Debug.Write("Message received: " + message);
+            MainWindow.main.Dispatcher.Invoke(new Action(delegate ()
+            {
+                MainWindow.main.receiveText.Text += Environment.NewLine + message;
+            }));
+        }
+   }
+}
---
+++ MQTTIota/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using MQTTIota;
+namespace MQTTIota
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal static MainWindow main;
+        public MainWindow()
+        {
+            main = this;
+            InitializeComponent();
+            MQTT client = new MQTT("iot.eclipse.org", "/IotaTransaction");
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
